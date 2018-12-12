e8a9fe90f9bba9e2c9c2fa7961ae5739cb455a55
lmor98 <seriesencasaoficial@gmail.com>
2018-12-12 19:59:32 +0100

    Pintamos la pieza en el tablero

--- js/codi.js
+++ js/codi.js
@@ -28,6 +28,7 @@ var jocTetris = {
     ],
     puntuacio: 0,
     puntuacioMax: 0,
+    nivell: 0,
     pecaBajando: new Array(),
     seguentPeca: new Array(),
     comptadorPeca: 0,
@@ -62,14 +63,30 @@ var jocTetris = {
             [0,0,0,0,0,0,0,0,0,0]
         ]
         puntuacio = 0;
-        pecaBajando = new Array();
-        seguentPeca = new Array();
+        var piezaAleatoria = GeneraPecaAleatoria();
+        pecaBajando = new Peca(piezaAleatoria[0], piezaAleatoria[1], 5, 5);
+        piezaAleatoria = GeneraPecaAleatoria();
+        seguentPeca = new Peca(piezaAleatoria[0], piezaAleatoria[1], 5, 5);
         comptadorPeca = 0;
-        intervarlTemps; //Falta ampliar
+        nivell = 0;
+        intervarlTemps = 0; //Falta ampliar
         //Calcular de forma aleatoria la següent peça que baixara
         //Gestio de la interacció del teclat
         //Moviment automatic que sera el intervalo de timepo que ira bajando la piez
 
+    },
+    pintarPiezaTablero: function(){
+        //Recorremos las filas de la pieza que bajara
+        for(var i = 0; i < pecaBajando.forma.length; i++){
+            //Recorremos las columnas de la pieza que abajara
+            for(var j = 0; j < pecaBajando.forma[i].length; j++){
+                //Comprobamos que esa pieza en esa columna y fila contenga un 1
+                if(pecaBajando.forma[i][j] == 1){
+                    //Pintamos en el tablero esa pieza 
+                    espai[pecaBajando.posicioX + i][pecaBajando.posicioY + j] = 1;
+                }
+            }
+        }
     }
 }
 
@@ -88,7 +105,7 @@ function GeneraPecaAleatoria (){
 
     var numeroAleatori = Math.round(Math.random()*6);                      
     return peces[numeroAleatori];     
-    }    
+}    
 
 
 //Creamos el objeto piezas
@@ -98,13 +115,11 @@ var Peca = function(forma, color, posicioX, posicioY){
     this.posicioX = posicioX;
     this.posicioY = posicioY;
 }
-//Generamos una pieza aleatoria
-var piezaAleatoria = GeneraPecaAleatoria();
-var pieza = new Peca(piezaAleatoria[0], piezaAleatoria[1]);
 
 //Creamos una pieza
 var p = new Peca(GeneraPecaAleatoria()[0], GeneraPecaAleatoria()[1], 5, 25);
-console.log(p);
+//console.log(p);
+
  
 //Funcion que desplazara la pieza a la derecha
 Peca.prototype.moverDerecha = function(){
@@ -129,14 +144,21 @@ Peca.prototype.moverIzqueirda = function(){
 
 //Pintamos la pieza
 Peca.prototype.pintarPieza = function(){
+    //Creamos un tabalero
     var resultat = "<table border='1'>";
+    //Recorremos las filas
     for (var i = 0; i < this.forma.length;i++){
-        resultat = resultat + "<tr>"
-        for (var j = 0; j<this.forma[i].length;j++) { 
+        //Creamos las fials
+        resultat = resultat + "<tr>";
+        //Recorremos cada columna de una fila en concreto
+        for (var j = 0; j < this.forma[i].length; j++) { 
+            //Creamos las celdas para las columnas
             resultat = resultat + "<td>";
-            if (this.forma[i][j]==1) { 
-                resultat=resultat+"1" 
+            //Comprovamos que en el array de la forma de la pieza si tien un 1 que pinte en el tablero un 1
+            if (this.forma[i][j] == 1) { 
+                resultat = resultat + "1"; 
             }
+            //Sino que pinte un 0
             else { 
                 resultat = resultat + "0" 
             };
@@ -145,7 +167,8 @@ Peca.prototype.pintarPieza = function(){
         resultat = resultat + "</tr>";
     }
     resultat = resultat + "</table>";
-    return resultat
+    //Devolvemos el tablero con todo el contenido dentro
+    return resultat;
 }; 
 
 //Funcion que rotara la pieza en sentido horario
@@ -168,27 +191,32 @@ Peca.prototype.rotarAntiHorari = function(){
     this.rotarSentitHorari();
     this.rotarSentitHorari();
 }
-pieza.rotarAntiHorari();
+
 
 //Funcion para hacer que la pieza baje
-Peca.prototype.moureAvall = function(posicioY){
+Peca.prototype.moureAvall = function(){
     //Esto hara que la pieza se desplace hacia abajo
     this.posicioY--;
+    console.log(p)
 }
-Peca.prototype.formaPeca = function(){}
+
+
+
 
 
 //Mostrem per pantalla l'espai de joc
 $(document).ready(function(){
     //Mostramos la pieza originalmente
-    document.getElementById("original").innerHTML = pieza.pintarPieza();
+    //document.getElementById("original").innerHTML = pieza.pintarPieza();
     //Giramos la pieza
-    pieza.rotarSentitHorari();
+    //pieza.rotarSentitHorari();
     //Mostramos la pieza girada
-    document.getElementById("girada").innerHTML = pieza.pintarPieza();
+    //document.getElementById("girada").innerHTML = pieza.pintarPieza();
 
     //Giramos la pieza en sentido antihorario
-    pieza.rotarAntiHorari();
+    //pieza.rotarAntiHorari();
+    jocTetris.inicialitzar();
+    jocTetris.pintarPiezaTablero();
 
     //Pintamos el tablero
     for(var i = 0; i < jocTetris.espai.length; i++){
@@ -198,4 +226,16 @@ $(document).ready(function(){
         }
         $("#tablero").append("</tr>")
     }
+
+    //Pintamos la puntuación
+    var punts = $("#puntuacion");
+    punts.append(jocTetris.puntuacio);
+
+    //Pintamos la puntiación máxima
+    var puntsMax = $("#puntuacionMax");
+    puntsMax.append(jocTetris.puntuacioMax);
+
+    //Pintamos el nivel del juego
+    var lvl = $("#nivell");
+    lvl.append(jocTetris.nivell);
 });
