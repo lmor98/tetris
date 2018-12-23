4f373ff5acc2456e60c232b8d9fa83adc4dfa83c
lmor98 <seriesencasaoficial@gmail.com>
2018-12-23 20:54:06 +0100

    La pieza se puede girar y mover

--- js/codi.js
+++ js/codi.js
@@ -75,57 +75,46 @@ var jocTetris = {
         setInterval(function(){jocTetris.automatic();}, jocTetris.intervarlTemps);
 
     },
+
+    //Funció per a que baixi la seguent peça aleatoria
     seguentPecaBajando: function(){
+        console.log("llega");
         jocTetris.piezaAleatoria = GeneraPecaAleatoria();
-        jocTetris.pecaBajando = new Peca(piezaAleatoria[0], piezaAleatoria[1], 20, 5);
+        jocTetris.pecaBajando = new Peca(jocTetris.piezaAleatoria[0], jocTetris.piezaAleatoria[1], 5, 5);
+        this.pintarPiezaTablero();
+        //Pintamos el tablero con el espacio
+        tableroTetris(jocTetris.espai);
     },
+
+    //Funció que baixarà la peça automaticament
     automatic: function(){
         var validacion = pecaBajando.validarPieza();
         if(validacion == true){
             //Hacemos que la pieza baje
             pecaBajando.posicioX++;
         }else{
-            jocTetris.seguentPeca();
+            console.log("<<<<<<<<")
+            jocTetris.seguentPecaBajando();
         }
 
         //Pintamos el tablero con el espacio
         tableroTetris(jocTetris.espai);
 
-        //jocTetris.validarPieza();
-
         //Recoremos el espacio de juego 
         for(var i = 0; i < jocTetris.espai.length; i++){
             for(var j = 0; j < jocTetris.espai[i].length; j++){
                 //Donde haya un 1 lo ponemos a 0
-                if(jocTetris.espai[i][j] == 1){
+                if(jocTetris.espai[i][j] == "F"){
                     jocTetris.espai[i][j] = 0;
                 }
             }
         }
-        
 
         //Pintamos la pieza en el tablero 
         jocTetris.pintarPiezaTablero();
     },
-    /*validarPieza: function(){
-        for(var i = 0; i < 4; i++){
-            for(var j = 0; j < 4; j++){
-                if(jocTetris.pecaBajando[i][j] = 1){
-                    if((jocTetris.pecaBajando.posicioX+i) < 0){
-                        return false;
-                    }
-                    if( ( (jocTetris.posicioX+i)<0) || ((jocTetris.pecaBajando.posicioY+j)>10)){
-                        return false;
-                    }
-                    if(jocTetris.espai[pecaBajando.posicioX+i][pecaBajando.posicioY+j]!=0){
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    },*/
 
+    //Funció que pinta la peça en el tauler de joc
     pintarPiezaTablero: function(){
         //Recorremos las filas de la pieza que bajara
         for(var i = 0; i < pecaBajando.forma.length; i++){
@@ -133,11 +122,18 @@ var jocTetris = {
             for(var j = 0; j < pecaBajando.forma[i].length; j++){
                 //Comprobamos que esa pieza en esa columna y fila contenga un 1
                 if(pecaBajando.forma[i][j] == 1){
-                    //Pintamos en el tablero esa pieza 
-                    this.espai[pecaBajando.posicioX + i][pecaBajando.posicioY + j] = 1;                    
+                    if(pecaBajando.validarPieza() == false){
+                        this.espai[pecaBajando.posicioX + i][pecaBajando.posicioY + j] = 1;
+                    }else{
+                        if(pecaBajando != null){
+                        //Pintamos en el tablero esa pieza 
+                        this.espai[pecaBajando.posicioX + i][pecaBajando.posicioY + j] = "F";  
+                        }
+                    }                 
                 }
             }
         }
+        tableroTetris(jocTetris.espai);
     },
 
     irAlaIzquierda: function(event){
@@ -146,7 +142,7 @@ var jocTetris = {
     irAlaDerecha: function(){}
 }
 
-//Creamos el objeto piezas
+//Creamos el objeto pieza
 var Peca = function(forma, color, posicioX, posicioY){
     this.forma = forma;
     this.color = color;
@@ -154,59 +150,11 @@ var Peca = function(forma, color, posicioX, posicioY){
     this.posicioY = posicioY;
 }
 
-//Creamos una pieza
-var p = new Peca(GeneraPecaAleatoria()[0], GeneraPecaAleatoria()[1], 2, 20);
-
-function colorPieza(){
-
-}
-
-//Creamos una variable donde guardaremos lo que nos devuelva la funcion para calcular de forma aleatoria la siguiente pieza
-function GeneraPecaAleatoria (){ 
-
-    var peces = [
-            [[[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]],"groc"],
-            [[[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]],"lila"],
-            [[[0,0,0,0],[0,1,1,0],[1,1,0,0],[0,0,0,0]],"verd"],
-            [[[0,0,0,0],[0,1,1,0],[0,0,1,1],[0,0,0,0]],"roig"],
-            [[[0,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,0,0]],"blau"],
-            [[[0,1,1,0],[0,1,0,0],[0,1,0,0],[0,0,0,0]],"taronga"],
-            [[[0,0,0,0],[1,1,1,0],[0,1,0,0],[0,0,0,0]],"morat"] 
-    ]
-
-    var numeroAleatori = Math.round(Math.random()*6);                      
-    return peces[numeroAleatori];     
-}    
-
-Peca.prototype.validarPieza = function(){
-    
-    /*for(var i = 0; i < 4; i++){
-        for(var j = 0; j < 4; j++){
-            if(jocTetris.pecaBajando[i][j] == 1){
-                if(pecaBajando[i][j] == 24){
-                    return false;
-                }
-                if((pecaBajando.posicioX+i) < 0){
-                    return false;
-                }
-                if( ( (this.posicioX+i)<0) || ((this.pecaBajando.posicioY+j)>10)){
-                    return false;
-                }
-                if(jocTetris.espai[pecaBajando.posicioX+i][pecaBajando.posicioY+j]!=0){
-                    return false;
-                }
-                
-            }
-        }
-    }*/
-    return true;
-}
- 
 //Funcion que desplazara la pieza a la derecha
 Peca.prototype.moverDerecha = function(){
-    console.log("entra1");
     //Comprobamos que al mover la pieza a la derecha no se salga del tablero
-    if( (x-1) > 0){
+    if( this.posicioY < 7){
+        this.posicioY++
         return true;
     }else{
         return false;
@@ -215,14 +163,22 @@ Peca.prototype.moverDerecha = function(){
 
 //Funcion que desplazara la pieza a la izquierda
 Peca.prototype.moverIzqueirda = function(){
-    console.log("entra2");
     //Comprobamos que al mover la pieza a la izquierda no se salga del tablero
-    if( (x+1) < 14 ){
-        x++;
+    if( this.posicioY > -1 ){
+        this.posicioY--;
         return true;
     }else{
         return false;
     }
+}
+
+//Funcion para hacer que la pieza baje
+Peca.prototype.moureAvall = function(){
+    if(pecaBajando.validarPieza() == true){
+        this.posicioX++;
+        jocTetris.puntuacio++;
+    }
+    
 }
 
 //Pintamos la pieza
@@ -252,7 +208,7 @@ Peca.prototype.pintarPieza = function(){
     resultat = resultat + "</table>";
     //Devolvemos el tablero con todo el contenido dentro
     return resultat;
-}; 
+}
 
 //Funcion que rotara la pieza en sentido horario
 Peca.prototype.rotarSentitHorari = function(){
@@ -268,6 +224,7 @@ Peca.prototype.rotarSentitHorari = function(){
     this.forma = formaNova;
 }
 
+//Funcion que rotara la pieza en sentido antihorario
 Peca.prototype.rotarAntiHorari = function(){
     //Llamamos a la funcion de girar a la derecha 3 veces que equivaldria a que si lo giramos a la izquierda
     this.rotarSentitHorari();
@@ -275,14 +232,50 @@ Peca.prototype.rotarAntiHorari = function(){
     this.rotarSentitHorari();
 }
 
+//Función que validara la pieza en el tablero para que no se salga del tablero 
+//Y para que al haber ya una pieza no se ponga encima
+Peca.prototype.validarPieza = function(){
+    //Recorremos la forma de la pieza
+    for(var i = 0; i < pecaBajando.forma.length; i++){
+        for(var j = 0; j < pecaBajando.forma[i].length; j++){
+            if(pecaBajando.forma[i][j] == 1){
+                //Comprobamos que la posicion no se salga del tablero a lo largo
+                if(this.posicioX+i == 24){
+                    return false;
+                }
+                //Comprobamos que la posicion no se salga del tablero a lo ancho
+                if((this.posicioY+j < 0) || (this.posicioY > 10)){
+                    return false;
+                }
+            }
+        }
+    }
 
-//Funcion para hacer que la pieza baje
-Peca.prototype.moureAvall = function(){
-
-    
+    return true;
 }
 
+//Creamos una variable donde guardaremos lo que nos devuelva la funcion para calcular de forma aleatoria la siguiente pieza
+function GeneraPecaAleatoria (){ 
 
+    var peces = [
+            [[[0,0,0,0],[0,1,1,0],[0,1,1,0],[0,0,0,0]],"groc"],
+            [[[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]],"lila"],
+            [[[0,0,0,0],[0,1,1,0],[1,1,0,0],[0,0,0,0]],"verd"],
+            [[[0,0,0,0],[0,1,1,0],[0,0,1,1],[0,0,0,0]],"roig"],
+            [[[0,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,0,0]],"blau"],
+            [[[0,1,1,0],[0,1,0,0],[0,1,0,0],[0,0,0,0]],"taronga"],
+            [[[0,0,0,0],[1,1,1,0],[0,1,0,0],[0,0,0,0]],"morat"] 
+    ]
+
+    var numeroAleatori = Math.round(Math.random()*6);                      
+    return peces[numeroAleatori];     
+}    
+
+function colorPieza(){
+
+}
+
+//Funcion que crea el tablero del tetris
 function tableroTetris(tablero){
     $("#tablero").empty();
     for(var i = 0; i < tablero.length; i++){
@@ -294,20 +287,9 @@ function tableroTetris(tablero){
     }
 }
 
-
-
 //Mostrem per pantalla l'espai de joc
 $(document).ready(function(){
-    //Mostramos la pieza originalmente
-    document.getElementById("original").innerHTML = p.pintarPieza();
-    //Giramos la pieza
-    p.rotarSentitHorari();
 
-    //Mostramos la pieza girada
-    document.getElementById("girada").innerHTML = p.pintarPieza();
-
-    //Giramos la pieza en sentido antihorario
-    //pieza.rotarAntiHorari();
     jocTetris.inicialitzar();
     jocTetris.pintarPiezaTablero();
     tableroTetris(jocTetris.espai);
@@ -326,6 +308,24 @@ $(document).ready(function(){
     var lvl = $("#nivell");
     lvl.append(jocTetris.nivell);
 
-    console.log(p)
-    document.getElementById("prueba").innerHTML = p.pintarPieza();
+    $("body").keydown(function(event){
+        if(event.keyCode == 38){ //Flecha hacia arriba
+            pecaBajando.rotarSentitHorari();
+        }
+        if(event.keyCode == 37){ //Flecha hacia la izquierda
+            pecaBajando.moverIzqueirda();
+        }
+        if(event.keyCode == 39){ //Flecha hacia la derecha
+            pecaBajando.moverDerecha();
+        }
+        if(event.keyCode == 40){ //Flecha hacia abajo
+            pecaBajando.moureAvall();
+        }
+        if(event.keyCode == 76){ //Para girar la pieza al otro lado
+            pecaBajando.rotarAntiHorari();
+        }
+        if(event.keyCode == 77){ //Para girar la pieza en setindo horario
+            pecaBajando.rotarSentitHorari();
+        }
+    })
 });
